f203fd9bb404077ba12878a383df3b800f1585b3
Will Taylor <william.h.taylor@stfc.ac.uk>
2022-02-10 16:29:24 +0000

    Create application project

---
+++ NXIngest/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NXIngest
+{
+    static class NXIngest
+    {
+        private const string vesuvio =
+            "C:/Users/rop61488/projects/work/LiveIngestEndToEndTests/test-data/simple-creation-tests/VESUVIO00045929.nxs";
+
+        private const string wish =
+            "C:/Users/rop61488/projects/work/LiveIngestEndToEndTests/test-data/simple-creation-tests/WISH00049859.nxs";
+
+        public static void Main(string[] args)
+        {
+            new NxsIngestor()
+                .IngestNexusFile(
+                    wish,
+                    "C:/FBS/Other/IngestExternalXmls/mapping_neutron.xml",
+                    "C:/Users/rop61488/test.xml"
+                );
+        }
+    }
+}
