41eb5d2fd7bbf5de67c3af30606b3603b96d05d3
Will Taylor <william.h.taylor@stfc.ac.uk>
2022-02-10 13:58:40 +0000

    Aggregation for datasets too big to fit in memory
    The key improvement over the previous hack version is iterating in the correct order, as explained in CalculateStepSize docstring

--- NXIngest/NxsFile.cs
+++ NXIngest/NxsFile.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using HDF5.NET;
 using log4net;
 
@@ -10,7 +9,9 @@ namespace NXIngest
     {
         private readonly ILog _log = LogManager.GetLogger(typeof(NxsFile));
         private readonly H5File _nxs;
-        private readonly Dictionary<string, AggregateValues> _aggregateCache = new();
+
+        private readonly Dictionary<string, AggregateValues> _aggregateCache =
+            new();
 
         public NxsFile(string path)
         {
@@ -33,6 +34,7 @@ namespace NXIngest
                     $"{ds.Space.Rank}. Ignoring element.");
                 return null;
             }
+
             var res = ds.Type.Class switch
             {
                 H5DataTypeClass.String => ds.ReadString()[0],
@@ -81,49 +83,119 @@ namespace NXIngest
             {
                 return _aggregateCache[path];
             }
-            var dataset = _nxs.Dataset(path);
-            var dims = dataset.Space.Dimensions;
 
-            const int uintsInAGb = 1_000_000_000 / 32;
-            if (dims.Length > 3)
-            {
-                throw new Exception("Can only aggregate 2d or 3d arrays, " +
-                    $"'{path}' is a {dims.Length}d array");
-            }
+            var dataset = _nxs.Dataset(path);
 
             AggregateValues res;
-            if (dims.Length < 3 || dims.Aggregate(1UL, (a, b) => a * b) < uintsInAGb)
+            if (dataset.Space.Rank is < 2 or > 3)
+            {
+                throw new Exception("Can only aggregate 2d or 3d arrays, " +
+                                    $"'{path}' is a {dataset.Space.Rank}d array");
+            }
+            else if (dataset.Space.Rank == 2)
             {
                 var arr = dataset.Read<uint>();
                 res = MatrixOperations.CalculateAggregates(arr);
             }
             else
             {
+                /*
+                 * Some of the datasets we have to aggregate don't fit in memory.
+                 * To work around this, we break the dataset up into 'slabs' that
+                 * will fit. Each slab is (x, y, z) in size, which is determined
+                 * by the algorithm described by CalculateStepSize.
+                 *
+                 * Note: (x, y, z) is equal to dims if the full dataset will fit
+                 * in the quarter GB memory we've allotted as the maximum, which
+                 * is the case for the majority of datasets. In these cases,
+                 * everything is read in a single iteration.
+                 */
+                var dims = dataset.Space.Dimensions;
                 var counts = new List<ArrayCounts>();
-                var numArrays = dims[^1];
-                const int atATime = 149;
-                var toGet = new[] { 0UL, 0UL, 0UL };
-                var arraySize = new[] { dims[0], dims[1], (ulong)atATime };
-                var outputSize = dims[0] * dims[1] * atATime;
-                for (var i = 0UL; i < numArrays; i++)
+                var (x, y, z) = CalculateStepSize((dims[0], dims[1], dims[2]));
+                var outputSize = x * y * z;
+ 
+                // For each step of (x, y, z), aggregate the counts of the dataset
+                for (var i = 0UL; i < dims[0]; i += x)
                 {
-                    toGet[2] = i * atATime;
-                    var fileSelection = new HyperslabSelection(
-                        3,
-                        toGet,
-                        arraySize);
-                    var memorySelection = new HyperslabSelection(0, outputSize);
-                    var arr = dataset.Read<uint>(fileSelection, memorySelection);
-                    var thisCount = MatrixOperations.GetArrayCounts(arr);
-                    Console.WriteLine($"{i}: {thisCount.Sum}");
-                    counts.Add(thisCount);
+                    for (var j = 0UL; j < dims[1]; j += y)
+                    {
+                        for (var k = 0UL; k < dims[2]; k += z)
+                        {
+                            var fileSelection = new HyperslabSelection(
+                                3,
+                                new[] { i, j, k },
+                                new[] { x, y, z });
+                            var memorySelection =
+                                new HyperslabSelection(0, outputSize);
+                            var arr = dataset.Read<uint>(fileSelection,
+                                memorySelection);
+                            var thisCount = MatrixOperations.GetArrayCounts(arr);
+                            counts.Add(thisCount);
+                        }
+                    }
                 }
 
+                // Combine the step counts into the final aggregate values
                 res = AggregateValues.FromCounts(counts);
             }
 
             _aggregateCache[path] = res;
             return res;
+        }
+
+        // Number of uints in 1/4 of a GB
+        private const ulong MaxSlabSize = 1073741824 / (4 * 4);
+
+        /// <summary>
+        /// Calculates the best step size for iterating through a dataset
+        ///
+        /// The step size must be a factor of the total size in all
+        /// dimensions, and a step must fit into the allotted memory. Each step
+        /// of size (x, y, z) take up x * y * z * sizeof(uint) bytes.
+        ///
+        /// The data is stored in memory in 'C order', meaning it's laid out
+        /// by the z axis first, then y axis, then x axis. This means we want to
+        /// maximize z first, then y, then x, to read as much contiguous data
+        /// as possible per slab.
+        /// </summary>
+        /// <param name="dims">The dimensions of the dataset</param>
+        /// <returns>The largest step that will fit into memory that's a factor
+        /// of dims</returns>
+        private static (ulong, ulong, ulong) CalculateStepSize(
+            (ulong, ulong, ulong) dims)
+        {
+            var (xSize, ySize, zSize) = dims;
+            if (zSize > MaxSlabSize)
+            {
+                throw new Exception(
+                    "Can't even fit one z slice into memory, giving up");
+            }
+
+            while (xSize * ySize * zSize > MaxSlabSize)
+            {
+                xSize /= SmallestFactor(xSize);
+
+                // Keep trying to make x smaller if needed before moving onto y
+                if (xSize != 1) continue;
+
+                while (ySize * zSize > MaxSlabSize)
+                {
+                    ySize /= SmallestFactor(ySize);
+                }
+            }
+
+            return (xSize, ySize, zSize);
+        }
+
+        private static ulong SmallestFactor(ulong n)
+        {
+            for (var i = 2UL; i < n; i++)
+            {
+                if (n % i == 0) return i;
+            }
+
+            return n;
         }
     }
 }
